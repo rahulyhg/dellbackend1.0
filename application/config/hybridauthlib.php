d4298ba5d89c2b553de778fb9e925b22b491b878
Chintan Shah <chintan@wohlig.com>
2015-03-19 16:07:14 +0530

    chaneg

--- application/config/hybridauthlib.php
+++ application/config/hybridauthlib.php
@@ -68,7 +68,7 @@ $config =
 		),
 
 		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
-		"debug_mode" => (ENVIRONMENT == 'development'),
+		"debug_mode" => false,
 
 		"debug_file" => APPPATH.'/logs/hybridauth.log',
 	);
